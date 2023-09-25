799e3bef3e19ecf229848c8762a8a32b50d24b1b
AlinaTantsura <alinatanst@gmail.com>
2023-09-25 21:15:29 +0300

    Task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(item =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+    src="${item.preview}" 
+    data-source="${item.original}"
+    alt="${item.description}"
+    ></a></li>`).join("");
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+
+gallery.addEventListener('click', selectImage);
+function selectImage(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+    instance.show();
+    
+    // instance.close();
+}
