7041d7acea5e91f94eabf5c8c91e9c6123ba8b1e
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-09-26 22:10:25 +0300

    Close-by-esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,6 +5,7 @@ console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
+
 const markup = galleryItems.map(item =>
     `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
@@ -20,10 +21,25 @@ gallery.insertAdjacentHTML('afterbegin', markup);
 gallery.addEventListener('click', selectImage);
 function selectImage(event) {
     event.preventDefault();
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}">
-`)
-    instance.show();
     
-    // instance.close();
+    if(event.target === event.currentTarget){   
+            return;
+    }
+    
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`);
+
+    instance.show(); 
+
+    if(instance.visible()){
+        document.addEventListener('keydown', closeModal);
+    }
+   
+    function closeModal(event){
+        if (event.key === "Escape"){
+            instance.close();
+            document.removeEventListener('keydown', closeModal);
+        }
+    }
+    
 }
